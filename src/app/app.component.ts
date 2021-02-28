d27a22092d4213905fe4500207a937888100c8d7
RukmalS92 <rukmsenarath@gmail.com>
2021-02-28 13:39:36 +0530

    fourth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+
+  afterloading : boolean = false;
+
   title = 'jnj';
+
+  ngAfterViewInit() {
+    
+  }
+
+  ngOnInit() {
+    this.afterloading = true;
+  }
 }
